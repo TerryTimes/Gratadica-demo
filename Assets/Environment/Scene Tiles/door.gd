817c7fe1ba0849668b5108d6af365f57cec6ebfb
Aidan W <460266@bsd48.org>
2024-05-09 12:49:32 -0700

    Map changes and door sprite updates.

--- Assets/Environment/Scene Tiles/door.gd
+++ Assets/Environment/Scene Tiles/door.gd
@@ -2,7 +2,10 @@ extends StaticBody2D
 class_name Door
 
 @export var key:Key
+@export var color:Color
 
 func _ready():
+	modulate = color
+	key.modulate = color
 	if !key.is_connected("collected", queue_free):
 		key.connect("collected", queue_free)
--- Scenes/UI/testingthebutton.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func _process(delta):
-	print("THE BUTTON IS RUNNING")
-
-func _pressed():
-	print("WAEOIH")
