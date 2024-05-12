40886717476396e71d5de6fa39b5c4144e39d51d
Dappro <aidanwignall2009@gmail.com>
2024-05-11 17:50:46 -0700

    Visual updates and master scene

---
+++ Scenes/Main Scenes/Master.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var open_on_menu: bool
+
+var menu = preload("res://Scenes/Main Scenes/main_menu.tscn")
+var main = preload("res://Scenes/Main Scenes/main.tscn")
+
+var current_scene = null
+
+func _ready():
+	if open_on_menu:
+		load_scene(menu)
+	else:
+		load_scene(main)
+
+func load_scene(scene):
+	if current_scene:
+		remove_child(current_scene)
+	current_scene = scene.instantiate()
+	add_child(current_scene)
--- Scenes/UI/phoslogo.gd
+++ Scenes/UI/phoslogo.gd
@@ -1,4 +1,15 @@
 extends TextureRect
 
+var flicker_chance = 0.1
+var flicker_size = 0.9
+var flicker_variation = 0.1
+
 func _ready():
 	custom_minimum_size.x = get_tree().root.size.x * 0.3
+
+func _process(delta):
+	if randf() < flicker_chance:
+		var brightness = randf_range(flicker_size - flicker_variation, flicker_size + flicker_variation)
+		modulate = Color(brightness,brightness,brightness,1)
+	else:
+		modulate = Color(1,1,1,1)
